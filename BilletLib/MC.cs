93369ae80f0bd8e5c10bda37726d043ee4b1ac50
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-08-30 12:23:06 +0200

    Lavet MC klassen public for at lave unittest
    lavet unit test af prisen samt hvilken string "MC" der bliver retunret

--- BilletLib/MC.cs
+++ BilletLib/MC.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BilletLib
 {
-    class MC
+  public  class MC
     {
         //props
         public String Nummerplade { get; set; }
---
+++ BilletLibTests/MCTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PrisTest125()
+        {
+            //arrange
+            MC MC = new MC();
+            //act
+            int pris = MC.Pris();
+            //assert
+            Assert.AreEqual(125, pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTestMC()
+        {
+            //arrange
+            MC MC = new MC();
+            //act
+            string Køretøj = MC.Køretøj();
+            //assert
+            Assert.AreEqual("MC", Køretøj);
+        }
+    }
+}
