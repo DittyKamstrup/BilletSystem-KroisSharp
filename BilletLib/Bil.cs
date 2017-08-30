c18cd48a3293cd155533155dedf52a7839085efc
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-08-30 12:01:48 +0200

    +*Bil +Props String Nummerplade Datime Dato
    + metoder
    Pris metode - fast pris
    Køretøj metode - hvilket køretøj er det

--- BilletLib/Bil.cs
+++ BilletLib/Bil.cs
@@ -8,5 +8,24 @@ namespace BilletLib
 {
     public class Bil
     {
+        //props
+        public string Nummerplade { get; set; }
+        public DateTime Dato { get; set; }
+
+        /// <summary>
+        /// prisen er fast på 240kr 
+        /// </summary>
+        /// <returns></returns>
+        public int Pris()
+        {
+            return 240;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+
+
     }
 }
