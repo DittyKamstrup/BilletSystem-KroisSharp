0efb984e41595d6fbf6f7ef0b7acbb6039e5ecd7
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-09-03 13:53:49 +0200

    opgave 6 done. tilføjet metode til at beregne brobizz samt test metode under køretøj test

--- BilletLib/Køretøj.cs
+++ BilletLib/Køretøj.cs
@@ -14,6 +14,10 @@ namespace BilletLib
         public abstract int Pris();
         public abstract string Typekøretøj();
 
+        /// <summary>
+        /// vi vil se om vores nummerplade er for stor. smider en execption hvis den er for lang.
+        /// </summary>
+        /// <returns></returns>
         public int NummerpladeLænge()
         {
             if (Nummerplade.Length > 7)
@@ -23,6 +27,19 @@ namespace BilletLib
             return Nummerplade.Length ;
         }
         
+        /// <summary>
+        /// vi beregner prisen ved først at finde ud af hvor meget 5% udgør af prisen MC/BIL
+        /// og finder så brobizzPrisen og smider den tilbage
+        /// </summary>
+        /// <returns></returns>
+        public int BrobizzRabat()
+        {
+            int rabat = Pris() / 100 * 5;
+            int brobizzPris = Pris() - rabat;
+
+
+            return brobizzPris;
+        }
 
     }
 }
--- BilletLibTests/KøretøjTests.cs
+++ BilletLibTests/KøretøjTests.cs
@@ -33,7 +33,19 @@ namespace BilletLib.Tests
             bil.Nummerplade = "12345678";
             int nummerpladelængde = bil.NummerpladeLænge();
             //vi har ikker assert med da vi forventer den thrower
-           
+
+        }
+
+        [TestMethod()]
+        public void BrobizzRabatTestTrue()
+        {
+            //arrange
+            Bil bil = new Bil();
+            //act
+            int bropris = bil.BrobizzRabat();
+            //assert
+            //bemærk vi bruger 230 da vi arbejder med int!!
+            Assert.AreEqual(230, bropris);
         }
     }
 }
